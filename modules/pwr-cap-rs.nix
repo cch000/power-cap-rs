860cf20826af573be4ddf622253a9709888625b4
Carlos <carloscamposherrera446@gmail.com>
2024-03-20 21:45:09 +0100

    modules: added option to only run the service when on battery

--- modules/pwr-cap-rs.nix
+++ modules/pwr-cap-rs.nix
@@ -1,4 +1,5 @@
 self: {
+  pkgs,
   config,
   lib,
   ...
@@ -43,25 +44,49 @@ in {
           Tctl Temperature Limit (Celsius)
         '';
       };
-    };
-  };
-
-  config = lib.mkIf cfg.enable {
-    systemd.services.pwr-cap-rs = {
-      description = "limit ryzen cpu power consumption when on power-saver";
-      wantedBy = ["default.target"];
-      serviceConfig = {
-        Type = "simple";
-        User = "root";
-        ExecStart = lib.getExe pwr-cap-rs;
+      onlyOnBattery = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+        description = ''
+          Wether to stop the service when the laptop is plugged in
+        '';
       };
     };
-
-    environment.etc."pwr-cap-rs.json".text = builtins.toJSON {
-      sus_pl = cfg.stapm-limit;
-      actual_pl = cfg.fast-limit;
-      avg_pl = cfg.slow-limit;
-      max_tmp = cfg.tctl-temp;
-    };
   };
+
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    {
+      systemd.services.pwr-cap-rs = {
+        description = "limit ryzen cpu power consumption when on power-saver";
+
+        serviceConfig = {
+          Type = "simple";
+          User = "root";
+          Restart = "always";
+          ExecStart = lib.getExe pwr-cap-rs;
+        };
+        wantedBy = ["default.target"];
+      };
+
+      environment.etc."pwr-cap-rs.json".text = builtins.toJSON {
+        sus_pl = cfg.stapm-limit;
+        actual_pl = cfg.fast-limit;
+        avg_pl = cfg.slow-limit;
+        max_tmp = cfg.tctl-temp;
+      };
+    }
+    (lib.mkIf cfg.onlyOnBattery {
+      systemd.services.pwr-cap-rs = {
+        unitConfig = {
+          ConditionACPower = "false";
+        };
+
+        services.udev.extraRules = let
+          unplug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="${pkgs.systemd}/bin/systemctl start pwr-cap-rs.service"'';
+          plug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${pkgs.systemd}/bin/systemctl stop pwr-cap-rs.service"'';
+        in
+          lib.strings.concatLines [unplug plug];
+      };
+    })
+  ]);
 }
