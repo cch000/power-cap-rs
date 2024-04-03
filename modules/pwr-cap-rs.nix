83f53bd6eb72d1961f6df6d211b75e86bcd1cc30
Carlos <carloscamposherrera446@gmail.com>
2024-04-03 14:35:41 +0200

    module: refactor && tweaks to the onlyOnBattery option

--- modules/pwr-cap-rs.nix
+++ modules/pwr-cap-rs.nix
@@ -5,48 +5,50 @@ self: {
   ...
 }: let
   inherit (self.packages.x86_64-linux) pwr-cap-rs;
+  inherit (lib) mkMerge mkOption mkIf types getExe strings;
   cfg = config.services.pwr-cap-rs;
 in {
   options = {
     services.pwr-cap-rs = {
-      enable = lib.mkOption {
+      enable = mkOption {
         default = false;
-        type = with lib.types; bool;
+        type = types.bool;
         description = ''
           "Run pwr-cap-rs as a systemd service"
         '';
       };
-      stapm-limit = lib.mkOption {
-        type = with lib.types; nullOr ints.unsigned;
+      stapm-limit = mkOption {
+        type = with types; nullOr ints.unsigned;
         default = null;
         description = ''
           Sustained Power Limit (mW)
         '';
       };
-      fast-limit = lib.mkOption {
-        type = lib.types.ints.unsigned;
-        default = 9000;
+      fast-limit = mkOption {
+        type = types.ints.unsigned;
+        default = null;
         description = ''
-          Actual Power Limit, cannot be null (mW)
+          Actual Power Limit (mW).
+          Cannot be null, you must set a value that suits your usecase.
         '';
       };
-      slow-limit = lib.mkOption {
-        type = with lib.types; nullOr ints.unsigned;
+      slow-limit = mkOption {
+        type = with types; nullOr ints.unsigned;
         default = null;
         description = ''
           Average Power Limit (mW)
         '';
       };
-      tctl-temp = lib.mkOption {
-        type = with lib.types; nullOr ints.unsigned;
+      tctl-temp = mkOption {
+        type = with types; nullOr ints.unsigned;
         default = null;
         description = ''
           Tctl Temperature Limit (Celsius)
         '';
       };
-      onlyOnBattery = lib.mkOption {
+      onlyOnBattery = mkOption {
         default = false;
-        type = lib.types.bool;
+        type = types.bool;
         description = ''
           Wether to stop the service when the laptop is plugged in
         '';
@@ -54,7 +56,7 @@ in {
     };
   };
 
-  config = lib.mkIf cfg.enable (lib.mkMerge [
+  config = mkIf cfg.enable (mkMerge [
     {
       systemd.services.pwr-cap-rs = {
         description = "limit ryzen cpu power consumption when on power-saver";
@@ -63,9 +65,8 @@ in {
           Type = "simple";
           User = "root";
           Restart = "always";
-          ExecStart = lib.getExe pwr-cap-rs;
+          ExecStart = getExe pwr-cap-rs;
         };
-        wantedBy = ["default.target"];
       };
 
       environment.etc."pwr-cap-rs.json".text = builtins.toJSON {
@@ -75,8 +76,12 @@ in {
         max_tmp = cfg.tctl-temp;
       };
     }
-    (lib.mkIf cfg.onlyOnBattery {
+    (mkIf (!cfg.onlyOnBattery) {
+      systemd.services.pwr-cap-rs.wantedBy = ["default.target"];
+    })
+    (mkIf cfg.onlyOnBattery {
       systemd.services.pwr-cap-rs = {
+        wantedBy = [];
         unitConfig = {
           ConditionACPower = "false";
         };
@@ -86,7 +91,7 @@ in {
         unplug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="${pkgs.systemd}/bin/systemctl start pwr-cap-rs.service"'';
         plug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${pkgs.systemd}/bin/systemctl stop pwr-cap-rs.service"'';
       in
-        lib.strings.concatLines [unplug plug];
+        strings.concatLines [unplug plug];
     })
   ]);
 }
